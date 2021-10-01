c9efc3e38f8d283f3f90a6cb94b3d01551ee6008
Andrea Colina <a.colina.2020@alumnos.urjc.es>
2021-10-01 15:58:40 +0200

    Add base project

---
+++ src/main/java/com/mastercloudapps/thesocialnetworkplanner/TheSocialNetworkPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.mastercloudapps.thesocialnetworkplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSocialNetworkPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSocialNetworkPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mastercloudapps/thesocialnetworkplanner/TheSocialNetworkPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastercloudapps.thesocialnetworkplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSocialNetworkPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
