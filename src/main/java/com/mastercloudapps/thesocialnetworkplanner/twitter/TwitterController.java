01eeb38bb0bc495cd1a20db6423323f6d3da48d6
Andrea Colina <a.colina.2020@alumnos.urjc.es>
2021-10-11 17:47:39 +0200

    Add TwitterController

---
+++ src/main/java/com/mastercloudapps/thesocialnetworkplanner/twitter/TwitterController.java
@@ -0,0 +1,52 @@
+package com.mastercloudapps.thesocialnetworkplanner.twitter;
+
+import com.mastercloudapps.thesocialnetworkplanner.twitter.exception.TweetNotFoundException;
+import com.mastercloudapps.thesocialnetworkplanner.twitter.exception.TwitterClientException;
+import com.mastercloudapps.thesocialnetworkplanner.twitter.exception.UnauthorizedTwitterClientException;
+import com.mastercloudapps.thesocialnetworkplanner.twitter.model.TweetResponse;
+import com.mastercloudapps.thesocialnetworkplanner.twitter.model.TweetRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Validated
+@RequestMapping(value = "/twitter")
+public class TwitterController {
+    private final TwitterService twitterService;
+
+    public TwitterController(TwitterService twitterService) {
+        this.twitterService = twitterService;
+    }
+
+    @GetMapping("/tweet/{id}")
+    public ResponseEntity<TweetResponse> getTweet(@PathVariable String id) throws TwitterClientException {
+        TweetResponse tweetResponse = this.twitterService.getTweet(id);
+        return ResponseEntity.ok(tweetResponse);
+    }
+
+    @PostMapping("/tweet")
+    public ResponseEntity<TweetResponse> postTweet(@RequestBody TweetRequest tweetRequest) throws TwitterClientException {
+        TweetResponse tweetResponse = this.twitterService.postTweet(tweetRequest);
+        return ResponseEntity.ok(tweetResponse);
+    }
+
+    @ExceptionHandler(TweetNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleTweetNotFoundException(TweetNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(TwitterClientException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleTwitterClientException(TwitterClientException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedTwitterClientException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<String> handleUnauthorizedTwitterClientException(UnauthorizedTwitterClientException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+}
