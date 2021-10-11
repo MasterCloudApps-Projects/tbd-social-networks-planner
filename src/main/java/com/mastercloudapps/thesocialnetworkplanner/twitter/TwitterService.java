880bf778c4ed8d0021702d5c4d07fcacfcfabda3
Andrea Colina <a.colina.2020@alumnos.urjc.es>
2021-10-11 22:28:49 +0200

    Changes in logic

--- src/main/java/com/mastercloudapps/thesocialnetworkplanner/twitter/TwitterService.java
+++ src/main/java/com/mastercloudapps/thesocialnetworkplanner/twitter/TwitterService.java
@@ -1,6 +1,7 @@
 package com.mastercloudapps.thesocialnetworkplanner.twitter;
 
 import com.mastercloudapps.thesocialnetworkplanner.twitter.client.TwitterClient;
+import com.mastercloudapps.thesocialnetworkplanner.twitter.exception.TwitterBadRequestException;
 import com.mastercloudapps.thesocialnetworkplanner.twitter.exception.TwitterClientException;
 import com.mastercloudapps.thesocialnetworkplanner.twitter.model.TweetResponse;
 import com.mastercloudapps.thesocialnetworkplanner.twitter.model.TweetRequest;
@@ -14,18 +15,28 @@ public class TwitterService {
 
     private final TwitterClient twitterClient;
 
-    public TwitterService(TwitterClient twitterClient){
+    public TwitterService(TwitterClient twitterClient) {
         this.twitterClient = twitterClient;
     }
 
     public TweetResponse getTweet(String tweetId) throws TwitterClientException {
         Status status = this.twitterClient.getTweet(tweetId);
-        return new TweetResponse(String.valueOf(status.getId()), status.getUser().getScreenName(), status.getText());
+
+        if (status != null) {
+            return new TweetResponse(String.valueOf(status.getId()), status.getUser().getScreenName(), status.getText());
+        } else {
+            throw new TwitterBadRequestException();
+        }
     }
 
     public TweetResponse postTweet(TweetRequest tweetRequest) throws TwitterClientException {
         Status status = this.twitterClient.postTweet(tweetRequest.getText());
-        return new TweetResponse(String.valueOf(status.getId()), status.getUser().getScreenName(), status.getText());
+
+        if (status != null) {
+            return new TweetResponse(String.valueOf(status.getId()), status.getUser().getScreenName(), status.getText());
+        } else {
+            throw new TwitterBadRequestException();
+        }
     }
 
 }
--- src/main/java/com/mastercloudapps/thesocialnetworkplanner/twitter/client/TwitterClient.java
+++ src/main/java/com/mastercloudapps/thesocialnetworkplanner/twitter/client/TwitterClient.java
@@ -4,62 +4,62 @@ import com.mastercloudapps.thesocialnetworkplanner.twitter.exception.TweetNotFou
 import com.mastercloudapps.thesocialnetworkplanner.twitter.exception.TwitterClientException;
 import com.mastercloudapps.thesocialnetworkplanner.twitter.exception.UnauthorizedTwitterClientException;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
 
 import java.util.Objects;
 
-@Component
+@Service
 @Log4j2
 public class TwitterClient {
     private final Twitter twitter;
 
-    public TwitterClient() {
-        this.twitter = new TwitterFactory().getInstance();
+    public TwitterClient(Twitter twitter) {
+        this.twitter = twitter;
     }
 
     public Status getTweet(String tweetId) throws TwitterClientException {
-        Status status = null;
-        try {
-            if (tweetId != null) {
+        Status status;
+        if (tweetId != null) {
+            try {
                 status = twitter.showStatus(Long.parseLong(tweetId));
-            } else {
-                log.info("Tweet id is empty.");
+                log.info("Showing @" + status.getUser().getScreenName() + "'s tweet.");
+                log.info("@" + status.getUser().getScreenName() + " - " + status.getText());
+                return status;
+            } catch (TwitterException te) {
+                log.info("Twitter [get-tweet] throw exception: " + te.getMessage());
+                if (Objects.equals(te.getStatusCode(), 404)) {
+                    throw new TweetNotFoundException();
+                }
+                throw new TwitterClientException();
             }
-            log.info("Showing @" + status.getUser().getScreenName() + "'s tweet.");
-            log.info("@" + status.getUser().getScreenName() + " - " + status.getText());
-        } catch (TwitterException te) {
-            log.info("Twitter [get-tweet] throw exception: " + te.getMessage());
-            if (Objects.equals(te.getStatusCode(), 404)) {
-                throw new TweetNotFoundException();
-            }
-            throw new TwitterClientException();
+        } else {
+            log.info("Tweet id is empty.");
+            return null;
         }
-
-        return status;
     }
 
     public Status postTweet(String text) throws TwitterClientException {
-        Status status = null;
-        try {
-            if (text != null) {
+        Status status;
+        if (text != null) {
+            try {
                 status = twitter.updateStatus(text);
-            } else {
-                log.info("Message for status is empty.");
-            }
-            log.info("Showing recently posted @" + status.getUser().getScreenName() + "'s tweet.");
-            log.info("@" + status.getUser().getScreenName() + " - " + status.getText());
-        } catch (TwitterException te) {
-            if (Objects.equals(te.getStatusCode(), 401)) {
+                log.info("Showing recently posted @" + status.getUser().getScreenName() + "'s tweet.");
+                log.info("@" + status.getUser().getScreenName() + " - " + status.getText());
+                return status;
+            } catch (TwitterException te) {
+                if (Objects.equals(te.getStatusCode(), 401)) {
+                    log.info("Twitter [post-tweet] throw exception: " + te.getMessage());
+                    throw new UnauthorizedTwitterClientException();
+                }
                 log.info("Twitter [post-tweet] throw exception: " + te.getMessage());
-                throw new UnauthorizedTwitterClientException();
+                throw new TwitterClientException();
             }
-            log.info("Twitter [post-tweet] throw exception: " + te.getMessage());
-            throw new TwitterClientException();
+        } else {
+            log.info("Message for status is empty.");
+            return null;
         }
-        return status;
     }
 }
