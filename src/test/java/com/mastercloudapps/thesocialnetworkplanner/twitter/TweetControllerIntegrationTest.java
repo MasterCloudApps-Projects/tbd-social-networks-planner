04fff3ac5235e520b88d4dffa538ade6a2e2f77a
Juan Manuel <sanblas2b.juanma@gmail.com>
2021-10-19 18:33:48 +0200

    Activate profile in Controller tests

--- src/test/java/com/mastercloudapps/thesocialnetworkplanner/twitter/TweetControllerIntegrationTest.java
+++ src/test/java/com/mastercloudapps/thesocialnetworkplanner/twitter/TweetControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("dev")
 public class TweetControllerIntegrationTest {
 
     @Autowired
