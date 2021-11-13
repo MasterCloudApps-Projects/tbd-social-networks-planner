39d553a7bedad2deafbb56ddf6c0bf2a66acd222
Andrea Colina <a.colina.2020@alumnos.urjc.es>
2021-11-13 17:04:53 +0100

    Add endpoints

--- src/main/java/com/mastercloudapps/thesocialnetworkplanner/instagram/controller/InstagramController.java
+++ src/main/java/com/mastercloudapps/thesocialnetworkplanner/instagram/controller/InstagramController.java
@@ -2,22 +2,23 @@ package com.mastercloudapps.thesocialnetworkplanner.instagram.controller;
 
 import com.mastercloudapps.thesocialnetworkplanner.instagram.exception.InstagramBadRequestException;
 import com.mastercloudapps.thesocialnetworkplanner.instagram.exception.InstagramException;
-import com.mastercloudapps.thesocialnetworkplanner.instagram.model.DeviceLoginResponse;
+import com.mastercloudapps.thesocialnetworkplanner.instagram.model.InstagramDeviceLoginResponse;
+import com.mastercloudapps.thesocialnetworkplanner.instagram.model.InstagramMediaResponse;
+import com.mastercloudapps.thesocialnetworkplanner.instagram.model.InstagramPostInfoResponse;
 import com.mastercloudapps.thesocialnetworkplanner.instagram.service.InstagramService;
 import com.mastercloudapps.thesocialnetworkplanner.resource.model.ResourceResponse;
 import com.mastercloudapps.thesocialnetworkplanner.resource.service.ResourceService;
 import lombok.extern.log4j.Log4j2;
+import org.ff4j.FF4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
+
+import static com.mastercloudapps.thesocialnetworkplanner.ff4jconfig.FeatureFlagsInitializer.FEATURE_GET_POST_INFO;
 
 @RestController
 @Validated
@@ -27,16 +28,18 @@ public class InstagramController {
 
     private final InstagramService instagramService;
     private final ResourceService resourceService;
+    private final FF4j ff4j;
 
-    public InstagramController(InstagramService instagramService, ResourceService resourceService) {
+    public InstagramController(InstagramService instagramService, ResourceService resourceService, FF4j ff4j) {
         this.instagramService = instagramService;
         this.resourceService = resourceService;
+        this.ff4j = ff4j;
     }
 
     @GetMapping("/login")
     public ResponseEntity<String> login() throws InstagramException {
-        DeviceLoginResponse deviceLoginResponse = this.instagramService.login();
-        return ResponseEntity.ok("Enter this code " + deviceLoginResponse.getUserCode() + " on " + deviceLoginResponse.getVerificationUri());
+        InstagramDeviceLoginResponse instagramDeviceLoginResponse = this.instagramService.login();
+        return ResponseEntity.ok("Enter this code " + instagramDeviceLoginResponse.getUserCode() + " on " + instagramDeviceLoginResponse.getVerificationUri());
     }
 
     @GetMapping("/auth")
@@ -52,6 +55,22 @@ public class InstagramController {
                                        @RequestParam(value = "image") MultipartFile multipartFile) throws InstagramException {
         ResourceResponse resource = this.resourceService.createImage(multipartFile);
         return ResponseEntity.ok(this.instagramService.post(resource, caption));
+    }
+
+    @GetMapping("/post/{id}")
+    public ResponseEntity getPostInfo(@PathVariable("id") @NotNull String id) throws InstagramException {
+        if (ff4j.check(FEATURE_GET_POST_INFO)) {
+            return ResponseEntity.ok(this.instagramService.getPostInfo(id));
+        }
+        return ResponseEntity.ok("TBD");
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity getAllMedia() throws InstagramException {
+        if (ff4j.check(FEATURE_GET_POST_INFO)) {
+            return ResponseEntity.ok(this.instagramService.getAllMedia());
+        }
+        return ResponseEntity.ok("TBD");
     }
 
     @ExceptionHandler(InstagramException.class)
