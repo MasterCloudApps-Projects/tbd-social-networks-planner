1e14d01cd2aab52324293809d0c967cb5ed9d344
Andrea Colina <a.colina.2020@alumnos.urjc.es>
2021-11-17 19:11:27 +0100

    Create Instagram Client interface

---
+++ src/main/java/com/mastercloudapps/thesocialnetworkplanner/instagram/client/InstagramClient.java
@@ -0,0 +1,19 @@
+package com.mastercloudapps.thesocialnetworkplanner.instagram.client;
+
+import com.mastercloudapps.thesocialnetworkplanner.instagram.exception.InstagramException;
+import com.mastercloudapps.thesocialnetworkplanner.instagram.model.InstagramDeviceLoginResponse;
+import com.mastercloudapps.thesocialnetworkplanner.instagram.model.InstagramMediaResponse;
+import com.mastercloudapps.thesocialnetworkplanner.instagram.model.InstagramPostInfoResponse;
+import com.mastercloudapps.thesocialnetworkplanner.resource.model.ResourceResponse;
+
+public interface InstagramClient {
+    InstagramDeviceLoginResponse login() throws InstagramException;
+
+    String authenticate() throws InstagramException;
+
+    String post(ResourceResponse resource, String caption) throws InstagramException;
+
+    InstagramPostInfoResponse getPostInfo(String id) throws InstagramException;
+
+    InstagramMediaResponse getAllMedia() throws InstagramException;
+}
